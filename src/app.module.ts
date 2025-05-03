3ca706433d7a7437d827f732de313dd6020d0c58
Aa <aa@example.com>
2025-05-03 15:56:14 +0300

    initial

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { PrismaModule } from './prisma/prisma.module';
+import { ChatModule } from './chat/chat.module';
 
 @Module({
   imports: [
@@ -8,6 +9,7 @@ import { PrismaModule } from './prisma/prisma.module';
       isGlobal: true,
     }),
     PrismaModule,
+    ChatModule,
   ],
 })
 export class AppModule {}
